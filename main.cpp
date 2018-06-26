1e80c271d88cafb85bc2dfed7a39249c3b1a01aa
Denis Boyda <boyda_d@mail.ru>
2018-06-26 15:13:28 +1000

    Dynamic lib is added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+#include "myDynLib.h"
+
 int main(int argc, char** argv)
 {
-	std::cout<<"Hello, World!"<<std::endl;
+	std::cout<<"Hello, World!\n";
+
+  std::cout<<fDyn();
+
+  std::cout<<std::endl;
 	return 0;
 }
---
+++ myDynLib.cpp
@@ -0,0 +1,6 @@
+#include "myDynLib.h"
+
+const char* fDyn()
+{
+  return "Hello from Dynamic lib\n";
+}
