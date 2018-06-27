78053a425bace605216ac2296acfead0a47e413b
Denis Boyda <boyda_d@mail.ru>
2018-06-27 16:12:40 +1000

    corretions

--- myDynLib.cpp
+++ myDynLib.cpp
@@ -11,7 +11,6 @@ const string version()
 {
   const string str = to_string(PROJECT_VERSION_MAJOR) + "."
                      + to_string(PROJECT_VERSION_MINOR) + "."
-                     + to_string(PROJECT_VERSION_PATCH) + "."
-                     + to_string(PROJECT_VERSIOM_BUILD);
+                     + to_string(PROJECT_VERSION_PATCH);
   return str;
 }
